1db57b2131c7f96bff5e51ad98ccda5b90fb078b
Mathias Karstädt <mathias.karstaedt@gmail.com>
2017-03-03 16:43:02 +0100

    add test with middleware for 100% test coverage

--- test/index.js
+++ test/index.js
@@ -1,17 +1,33 @@
 const { expect, MickrigTestCase } = require('mickrigtest')
 const { applyMiddleware } = require('../dist/micro-middleware.js')
 
+const req = { request: true }
+const res = { response: true }
+
+const myApp = async (req, res) => 
+    "hello world"
+
 testcase = new MickrigTestCase("micro-middleware")
+testcase.test("should apply undefined middleware", async function() {
+    let app = applyMiddleware(myApp)
+    let result = await app(req, res)
+    expect(result).to().be("hello world")
+})
 testcase.test("should apply empty middleware", async function() {
     let middleware = []
-    let req = { request: true }
-    let res = { response: true }
-
-    let myApp = async (req, res) => 
-        "hello world"
     let app = applyMiddleware(myApp, middleware)
     let result = await app(req, res)
     expect(result).to().be("hello world")
+})
+testcase.test("should apply simple middleware", async function() {
+    const universe = fn => async function process (...args) {
+        let data = await fn.apply(null, args)
+        return data.replace(/world/, 'universe')
+    }
+    let middleware = [universe]
+    let app = applyMiddleware(myApp, middleware)
+    let result = await app(req, res)
+    expect(result).to().be("hello universe")
 })
 
 const messages = testcase.run();

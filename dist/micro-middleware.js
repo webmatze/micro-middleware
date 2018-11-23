5157e083038697d53a81c7c9cc2e8f7e0d527c64
Freddy Alfaro <falfaro1@bancoestado.cl>
2018-11-23 16:44:22 -0300

    Change es5 to es6 throw babel

--- dist/micro-middleware.js
+++ dist/micro-middleware.js
@@ -2,12 +2,8 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-function applyMiddleware(service) {
-  var middleware = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
-
-  return middleware.reduce(function (fn, nextMiddleware) {
-    return nextMiddleware(fn);
-  }, service);
+function applyMiddleware(service, middleware = []) {
+  return middleware.reduce((fn, nextMiddleware) => nextMiddleware(fn), service);
 }
 
 exports.applyMiddleware = applyMiddleware;
